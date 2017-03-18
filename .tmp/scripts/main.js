3723f8bc1d9f6f4e40328354579b546314240517
shyamg91 <shyamg7777@gmail.com>
2017-03-18 21:14:37 +1300

    First

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,16 @@
+function calculateTime() {
+  var now = moment();
+  var weddingDate = moment('2017-11-30');
+  var days = weddingDate.diff(now, 'days');
+  var hours = weddingDate.diff(now, 'hours');
+  var minutes = weddingDate.diff(now, 'minutes');
+  var seconds = weddingDate.diff(now, 'seconds');
+
+  $('#days').val(days);
+  $('#minutes').val(minutes);
+  $('#hours').val(hours);
+  $('#seconds').val(seconds);
+}
+
+setInterval(calculateTime, 1000);
+//# sourceMappingURL=main.js.map
